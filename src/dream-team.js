1c04749099cd1f2b02c4e592a69fd79e935a595d
valeryshel <valeryshel@gmail.com>
2021-02-25 17:14:54 +0300

    feat: implement what-season task solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if (Array.isArray(members) === false) return false;
+  if (Array.isArray(members) === false) return false; // проверка является ли members массивом
 
-  let dreamTeamName = '';
+  let dreamTeamName = ''; // переменная для имени команды
 
   members.forEach(element => {
-    if(typeof(element) === 'string') {
-      dreamTeamName += element.trim()[0];
+    if(typeof(element) === 'string') { // проверка на тип данных
+      dreamTeamName += element.trim()[0]; // [0] элемент строки добавляем в dreamTeamName + обрезаем пробелы
     }
   });
-  return dreamTeamName.toUpperCase().split('').sort().join('');
+  return dreamTeamName.toUpperCase().split('').sort().join(''); // верхний регистр -> разделяем по эл-там -> сортировка -> соединяем
 };
